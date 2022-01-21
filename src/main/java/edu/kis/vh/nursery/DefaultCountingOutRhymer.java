f4a89ce9545c2c2121ffbe1734b1ab570ebce24b
Wojtek <wojciech.chmielewski@op.pl>
2022-01-21 14:07:17 +0100

    1.4 stale 1 zastąpienie literałów za pomocą extract

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public int total = -1;
-    private int[] numbers = new int[12];
+    public static final int negative = -1;
+    public static final int sizeOfNumbers = 12;
+    public static final int full = 11;
+    public int total = negative;
+    private int[] numbers = new int[sizeOfNumbers];
 
     public void countIn(int in) {
         if (!isFull())
@@ -11,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == negative;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == full;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return negative;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return negative;
         return numbers[total--];
     }
 
